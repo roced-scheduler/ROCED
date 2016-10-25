dca1aa0d8d6a883e34ab9f2ee16df0865181ae27
Frank Fischer <frank.fischer@student.kit.edu>
2016-10-25 18:45:06 +0200

    (HTCondor Requirement) Error handling - "Failed to fetch ads" will now REALLY be caught - Full condor_q string is required in ROCED in order to handle errors -> Change processing to generator expression(s) instead of GREP & AWK - Simplifies the code

--- src/ROCED/RequirementAdapter/HTCondorRequirementAdapter.py
+++ src/ROCED/RequirementAdapter/HTCondorRequirementAdapter.py
@@ -22,7 +22,6 @@ from __future__ import unicode_literals, absolute_import
 
 import getpass
 import logging
-import re
 
 from Core import Config
 from RequirementAdapter.Requirement import RequirementAdapterBase
@@ -47,8 +46,7 @@ class HTCondorRequirementAdapter(RequirementAdapterBase):
                          (condorStatusIdle, condorStatusRunning)
     # auto-format string: raw output, separated by comma
     _query_format_string = "-autoformat:r, JobStatus RequestCpus Requirements"
-    # gawk processing: ", " as separator, only output the first 2 strings, not the Requirements
-    _query_processing = "', ' '{print $1\", \"$2}'"
+
     _schedd_error_string = "Failed to fetch ads from:"
 
     def __init__(self):
@@ -93,59 +91,52 @@ class HTCondorRequirementAdapter(RequirementAdapterBase):
                              key=self.getConfig(self.configCondorKey))
 
         # Target.Requirements can't be filtered with -constraints since it would require ClassAd based regex matching.
-        # -> Filter requirement via "grep", if input it's supplied.
         # TODO: Find a more generic way to match resources/requirements (condor_q -slotads ??)
         # cmd_idle = "condor_q -constraint 'JobStatus == 1' -slotads slotads_bwforcluster " \
         #            "-analyze:summary,reverse | tail -n1 | awk -F ' ' " \
         #            "'{print $3 "\n" $4}'| sort -n | head -n1"
-        if len(self.getConfig(self.configCondorRequirement)) > 0:
-            grep_requirement_filter = "| grep -i '%s'" % self.getConfig(self.configCondorRequirement)
-        else:
-            grep_requirement_filter = ""
-
         constraint = "( %s ) && ( %s )" % (self._query_constraints, self.getConfig(self.configCondorConstraint))
 
-        cmd = ("condor_q -global -constraint '%s' %s %s | awk -F %s" %
-               (constraint, self._query_format_string, grep_requirement_filter, self._query_processing))
-
+        cmd = ("condor_q -global -stream-results -constraint '%s' %s" % (constraint, self._query_format_string))
         result = ssh.handleSshCall(call=cmd, quiet=True)
-
-        # Parse for failed Schedd queries ["-global" doesn't set return code]
-        if result[0] == 0 and self._schedd_error_string not in result[1]:
-            # result format: "2,1\n2,1\n1,1\n...\n" (JobStatus,RequestCpus)
-            # -> split lines into single list elements into 2 elements
-            # TODO: Generator expression(s)
-            condor_jobs = [job.split(",") for job in str(result[1]).splitlines()]
-            n_slots = 0
-            n_jobs_idle = 0
-            n_jobs_running = 0
-            for job in condor_jobs:
-                if any(condor_jobs[0]):
-                    status = int(job[0])
-                    cpus = int(job[1])
-                    n_slots += cpus
-                    if status == self.condorStatusIdle:
-                        n_jobs_idle += cpus
-                    elif status == self.condorStatusRunning:
-                        n_jobs_running += cpus
-
-            self.logger.debug("HTCondor queue: Idle: %d; Running: %d." % (n_jobs_idle, n_jobs_running))
-            self.logger.debug("[Job Status, CPUs])\n%s" % condor_jobs)
-
-            # this requires the machines variable to be listed twice in the config file
-            n_cores = - int(self.getConfig(self.configMachines)[self.getNeededMachineType()]["cores"])
-
-            # calculate the number of machines needed
-            self._curRequirement = - (n_slots // n_cores)
-
-            with Logging.JsonLog() as json_log:
-                json_log.addItem(self.getNeededMachineType(), "jobs_idle", n_jobs_idle)
-                json_log.addItem(self.getNeededMachineType(), "jobs_running", n_jobs_running)
-
-            return self._curRequirement
-        else:
+        if result[0] != 0:
             self.logger.warning("Could not get HTCondor queue status! %d: %s" % (result[0], result[2]))
             return None
+        elif self._schedd_error_string in result[1]:
+            self.logger.warning("condor_q request timed out.")
+            return None
+
+        queue_line = (entry.split(",", 3) for entry in str(result[1]).splitlines())
+        converted_line = ((int(status), int(cores), requirement) for status, cores, requirement in queue_line)
+        if self.getConfig(self.configCondorRequirement):
+            # TODO: We could use ClassAd bindings, to check requirement(s)
+            filtered_line = ((status, cores) for status, cores, requirement in converted_line
+                             if self.getConfig(self.configCondorRequirement) in requirement)
+        else:
+            filtered_line = ((status, cores) for status, cores, requirement in converted_line)
+
+        required_cpus_total = 0
+        required_cpus_idle_jobs = 0
+        required_cpus_running_jobs = 0
+        for job_status, requested_cpus in filtered_line:
+            required_cpus_total += requested_cpus
+            if job_status == self.condorStatusIdle:
+                required_cpus_idle_jobs += requested_cpus
+            elif job_status == self.condorStatusRunning:
+                required_cpus_running_jobs += requested_cpus
+
+        self.logger.debug("HTCondor queue: Idle: %d; Running: %d." %
+                          (required_cpus_idle_jobs, required_cpus_running_jobs))
+
+        # cores->machines: machine definition required for RequirementAdapter
+        n_cores = - int(self.getConfig(self.configMachines)[self.getNeededMachineType()]["cores"])
+        self._curRequirement = - (required_cpus_total // n_cores)
+
+        with Logging.JsonLog() as json_log:
+            json_log.addItem(self.getNeededMachineType(), "jobs_idle", required_cpus_idle_jobs)
+            json_log.addItem(self.getNeededMachineType(), "jobs_running", required_cpus_running_jobs)
+
+        return self._curRequirement
 
     def getNeededMachineType(self):
         # TODO: Handle multiple machine types!
