98127d89cf47233b40b0ebda6bd3e17263b6f474
matthias.schnepf <matthias.schnepf@kit.edu>
2017-09-04 10:08:52 +0200

    add setup.py

---
+++ src/ROCED/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+if __name__ == '__main__':
+    setup(
+        name='roced',
+        version='0.1.0',
+        author='ETP @ Karlsruhe Institute of Technology',
+        packages=find_packages(),
+    )
+
